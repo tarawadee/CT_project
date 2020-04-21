ed4f980d45a10ee1d0cac11dcc46ccd5d0e20ef7
watcharakan <job.2538@gmail.com>
2020-04-21 10:57:31 +0700

    UI

--- src/Layout/Post.js
+++ src/Layout/Post.js
@@ -1,10 +1,25 @@
 
 import React, { useState,useEffect } from 'react';
-import Card from "../components/UI/Card";
 import { getdataPost } from '../Firebase/Base'
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 import axios from 'axios';
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 345,
+    },
+    media: {
+        height: 140,
+    },
+});
 function Post(props) {
-
+    const classes = useStyles();
     const [data, setData] = useState({ covid: [] });
     const [ Post, setPost ] = useState({ post: [] });
 
@@ -74,13 +89,36 @@ function Post(props) {
                     {data.covid.Deaths}
                 </h5>
             </Card>
+
             <div>
                 { post.map( post => (
-                    <Card key={post}>
-                        <h1>{post.title}</h1>
-                        <p>{post.tag}</p>
-                        <p>{post.detail}</p>
+
+                    <Card className={classes.root} key={post}>
+                        <CardActionArea>
+                            <CardMedia
+                                className={classes.media}
+                                image="/static/images/cards/contemplative-reptile.jpg"
+                                title="Contemplative Reptile"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    {post.title}
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    {post.detail}
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                                Share
+                            </Button>
+                            <Button size="small" color="primary">
+                                Learn More
+                            </Button>
+                        </CardActions>
                     </Card>
+
                     ))}
             </div>
 
