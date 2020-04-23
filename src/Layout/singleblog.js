40dace88930e3379b76b8f99c1b2a7c0a88758a8
watcharakan <job.2538@gmail.com>
2020-04-24 01:41:55 +0700

    new ui

--- src/Layout/singleblog.js
+++ src/Layout/singleblog.js
@@ -1,64 +1,62 @@
 import React, {useEffect, useState} from 'react';
 import { getpostbyid } from '../Firebase/Base'
 import { useLocation } from 'react-router-dom';
-import CardMedia from '@material-ui/core/CardMedia';
-
+import Container from '@material-ui/core/Container';
 import {createMuiTheme, makeStyles} from '@material-ui/core/styles';
 import firebase from "firebase";
-import Card from "@material-ui/core/Card";
-import cx from "clsx";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
-
-
-
-const customTheme = createMuiTheme({
-    palette: { primary: { main: '#ff5252' } },
-});
-
-const config = {
-    sidebar: {
-        anchor: 'left',
-        width: 256,
-        variant: 'persistent',
-        collapsible: true,
-        collapsedWidth: 64,
-    },
-    header: {
-        position: 'relative',
-        offsetHeight: 64,
-        clipped: false,
-        persistentBehavior: 'fit',
-    },
-    content: {
-        persistentBehavior: 'fit',
-    },
-    footer: {
-        persistentBehavior: 'flexible',
-    },
-};
-const useStyles = makeStyles({
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+const useStyles = makeStyles((theme) => ({
     root: {
-        maxWidth: 345,
+
     },
     media: {
-        height: 140,
+        height: 0,
+        paddingTop: '56.25%', // 16:9
     },
-    card: {
-        borderRadius: 12,
-        minWidth: 256,
-        textAlign: 'center',
+    expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
     },
-});
-function Singleblog(props) {
+    expandOpen: {
+        transform: 'rotate(180deg)',
+    },
+    avatar: {
+        backgroundColor: red[500],
+    },
+}));
 
+
+
+function Singleblog(props) {
+    const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
     const location = useLocation();
     const { match } = props;
     const [Params, setParams] = useState('');
     const [Post, setPost] = useState('');
     let { documentId } = match.params
-    const classes = useStyles();
+
     useEffect(() => {
         getpostbyid(documentId).then((docRef) => {
             setPost(docRef.data())
@@ -68,29 +66,40 @@ function Singleblog(props) {
     });
 
     return (
-        <div>
-            <Card className={cx(classes.card)}  style={{marginTop:5}}>
 
-                <CardActionArea>
+        <Container fixed style={{marginTop:10}}>
+            <Card >
 
-                    <CardMedia
-                        className={classes.media}
-                        image={Post.url}
-                        title="Contemplative Reptile"
-                    />
-                    <CardContent>
-                        <h1>{Post.title}</h1>
-                        <p>{Post.detail}</p>
-                    </CardContent>
-                </CardActionArea>
+                <CardMedia
+                    className={classes.media}
+                    image={Post.url}
+                    title="Paella dish"
+                />
+                <CardHeader
+                    avatar={
+                        <Avatar aria-label="recipe" className={classes.avatar} src={Post.photoURL}>
 
+                        </Avatar>
+                    }
 
-
-
-
+                    title={Post.displayName}
+                    // subheader={Post.created}
+                />
+                <CardContent>
+                    <h1>{Post.title}</h1>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                       <p>{Post.detail}</p>
+                    </Typography>
+                </CardContent>
 
             </Card>
-        </div>
+
+        </Container>
+
+
+
+
+
     );
 }
 
